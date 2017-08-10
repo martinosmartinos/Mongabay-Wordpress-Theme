fe2dfae7563e6f1a3050bc3c7ead0a785e836379
martinosmartinos <martinosmartinos@gmail.com>
2017-08-10 22:39:17 +0200

    Mobile devices will load only one featured image

--- partials/section-slider.php
+++ partials/section-slider.php
@@ -1,6 +1,8 @@
 <?php
+    $postnumber = 4;
+    if(wp_is_mobile()) $postnumber = 1;
     $args = array(
-    	'posts_per_page' => '4',
+    	'posts_per_page' => $postnumber,
         'meta_query' => array(
             array(
                'key' => 'featured_as',
@@ -33,7 +35,7 @@
             	}
             ?>
             <?php if ($i == 1): ?>
-            	<div class="<?php echo $slideclass; ?>" style="background: url(<?php the_post_thumbnail_url('medium'); ?>);background-size: cover;">
+            	<div class="<?php echo $slideclass; ?>" style="background: url(<?php the_post_thumbnail_url('medium'); ?>);background-size: cover; background-position: 50% 50%;">
                     <a href="<?php the_permalink(); ?>">
                         <div class="slider-headline responsive-title"><?php the_title(); ?></div>
                     </a>
@@ -42,21 +44,21 @@
             <?php if ($i == 2): ?>
                 <div class="clearfix"></div>
                 <div class="col hidden-md-down">
-                    <div class="<?php echo $slideclass; ?>" style="background: url(<?php the_post_thumbnail_url('medium'); ?>);background-size: cover;border-left: 5px solid #fff;border-bottom: 5px solid #fff;">
+                    <div class="<?php echo $slideclass; ?>" style="background: url(<?php the_post_thumbnail_url('medium'); ?>);background-size: cover;border-left: 5px solid #fff;border-bottom: 5px solid #fff;background-position: 50% 50%;">
                         <a href="<?php the_permalink(); ?>">
                             <div class="slider-headline responsive-title"><?php the_title(); ?></div>
                         </a>
                     </div>
             <?php endif; ?>
             <?php if ($i == 3): ?>
-                    <div class="<?php echo $slideclass; ?>" style="background: url(<?php the_post_thumbnail_url('medium'); ?>);background-size: cover;border-left: 5px solid white;position: absolute;left: 0;top: 50%;">
+                    <div class="<?php echo $slideclass; ?>" style="background: url(<?php the_post_thumbnail_url('medium'); ?>);background-size: cover;border-left: 5px solid white;position: absolute;left: 0;top: 50%;background-position: 50% 50%;">
                         <a href="<?php the_permalink(); ?>">
                             <div class="slider-headline responsive-title"><?php the_title(); ?></div>
                         </a>
                     </div>
             <?php endif; ?>
             <?php if ($i == 4): ?>
-                    <div class="<?php echo $slideclass; ?>" style="background: url(<?php the_post_thumbnail_url('medium'); ?>);background-size: cover;position: absolute;right: 0;top: 50%;border-left: 5px solid #fff">
+                    <div class="<?php echo $slideclass; ?>" style="background: url(<?php the_post_thumbnail_url('medium'); ?>);background-size: cover;position: absolute;right: 0;top: 50%;border-left: 5px solid #fff;background-position: 50% 50%;">
                         <a href="<?php the_permalink(); ?>">
                             <div class="slider-headline responsive-title"><?php the_title(); ?></div>
                         </a>
