fe7cbc53d968c7964ea9326cfa98a0e4ab840246
martinosmartinos <martinosmartinos@gmail.com>
2017-08-15 14:45:57 +0200

    Include svg logo in featured header

--- header-featured.php
+++ header-featured.php
@@ -30,7 +30,8 @@
 	<header class="header fixed-top header-small" role="banner">
 		<?php get_template_part( 'partials/navigation', 'featured' ); ?>
 		<div class="logo-small" style="">
-			<a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo/mongabay_logo_black.png" class="mongabay-logo"></a>
+			echo '<svg width="120" height="26" aria-label="Mongabay">';
+			<a href="<?php echo home_url(); ?>"><svg width="120" height="26" aria-label="Mongabay"><img xlink:href="<?php echo get_template_directory_uri(); ?>/img/logo/mongabay_logo_black.svg" src="<?php echo get_template_directory_uri(); ?>/img/logo/mongabay_logo_black.png" class="mongabay-logo" width="120" height="26" alt="Mongabay"></svg></a>
 		</div>
 		<div class="social">
 			<?php get_template_part( 'partials/section', 'social' ); ?>
