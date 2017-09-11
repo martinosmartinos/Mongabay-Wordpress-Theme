76142c9fa63aa5ae55caa307fc12bd2f935e6bee
martinosmartinos <martinosmartinos@gmail.com>
2017-09-11 20:50:06 +0200

    Add topics to search results

--- search.php
+++ search.php
@@ -4,6 +4,34 @@
             <div id="main" class="col-lg-8">
                 <div class="tag-line">
                     <h1><?php echo sprintf( __( '%s Search Results for ', 'mongabay' ), $wp_query->found_posts ); echo '"'.get_search_query().'"'; ?></h1>
+                    <?php
+                        function get_tax_by_search($search_text) {
+
+                            $args = array(
+                                'taxonomy'      => array( 'topic' ),
+                                'orderby'       => 'id', 
+                                'order'         => 'ASC',
+                                'hide_empty'    => true,
+                                'fields'        => 'all',
+                                'name__like'    => $search_text
+                            ); 
+
+                            $terms = get_terms( $args );
+                            $count = count( $terms );
+                            if($count > 0) {
+                                _e ( 'Related topic pages:', 'mongabay' );
+                                echo '&nbsp;';
+                                foreach ($terms as $term) {
+                                    echo "<a href='".get_term_link( $term )."'>".$term->name."</a>";
+                                    if (next( $terms )) {
+                                        echo ', ';
+                                    }
+                                }
+                            }
+
+                        }
+                        get_tax_by_search( get_search_query() );
+                    ?>
                 </div>
                 <section>
 
