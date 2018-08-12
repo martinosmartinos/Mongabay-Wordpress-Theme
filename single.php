fb9f7fc7ce960027d11d7fd2994132518e6eb860
martinosmartinos <martinosmartinos@gmail.com>
2018-08-12 23:48:16 +0200

    Revised translated/adopted byline logic

--- single.php
+++ single.php
@@ -36,13 +36,13 @@
         <div class="single-article-meta">
             <?php if($commentary == '1' || $commentary == 'yes') _e('Commentary ', 'mongabay'); if($analysis == '1' || $analysis == 'yes') _e('Analysis ', 'mongabay'); _e('by ', 'mongabay'); ?><?php echo get_the_term_list( $post_id, 'byline', '', ', ', '' ); ?><?php _e(' on ', 'mongabay'); ?><?php the_time('j F Y'); ?>
             <?php
-                if (!empty($translator) || !empty($adaptor)) {
+                if ($translated_adapted == 'adapted' || $translated_adapted == 'translated') {
 
                         if ($translated_adapted == 'adapted' && !empty($adaptor)) {
                             $string_title = 'Adapted by';
                             $translator_adaptor = $adaptor;
                         }
-                        elseif (!empty($translator)) {
+                        elseif ($translated_adapted == 'translated' && !empty($translator)) {
                             $string_title = 'Translated by';
                             $translator_adaptor = $translator;
                         }
